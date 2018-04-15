37c1d47a25ad3d64cb0dd66b5fb6708cf21d4d4d
David Swearingen <swearingendw@mac.com>
2018-04-15 07:34:09 -0400

    removed old files

--- testing.js
+++
@@ -1,8 +0,0 @@
-// this works, it sends a message to my app server.js in node-and-socket-example directory which then
-// receives the message and does something.
-
-var socket = require('socket.io-client')('http://localhost:8001');
-socket.on('connect', function(){console.log('connect')});
-socket.on('event', function(data){});
-socket.on('disconnect', function(){});
-socket.emit('hey', 'success message');
